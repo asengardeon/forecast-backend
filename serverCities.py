e6284a67e25ccab7f6fe7e1d1d6fdf3dbb0e2bf0
Leandro Vilson Battisti <asengardeons@hotmail.com>
2019-03-30 14:34:41 -0300

    fixes to cors support

--- serverCities.py
+++ serverCities.py
@@ -2,27 +2,27 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from flask_jsonpify import jsonify
+from flask_cors import CORS
+
 
 db_connect = create_engine('sqlite:///forecast.db')
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
 class Cities(Resource):
     def get(self):
         conn = db_connect.connect()  # connect to database
-        query = conn.execute("select * from city")  # This line performs query and returns json result
-        return {'cities': [i[0] for i in query.cursor.fetchall()]}  # Fetches first column that is Employee ID
+        query = conn.execute("select * from city")
+        return {'cities': [{"id": i[0], "name": i[1]}for i in query.cursor.fetchall()]}
 
     def post(self):
         conn = db_connect.connect()
         rId = request.form['id']
         rName = request.form['name']
         conn.execute("insert into City values (?, ?)", (rId, rName))  # This line performs query and returns json result
-
-        query = conn.execute("select * from city where City =%d " % int(rId))
-        result = {'city': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
-        return jsonify(result)
+        return {"id": rId, "name": rName}
 
 
 class Cities_Name(Resource):
